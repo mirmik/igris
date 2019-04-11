eb07156ce21e378b22711b3605772783929c4a9e
mirmik <mirmikns@yandex.ru>
2019-04-11 12:32:03 +0300

    'types_extension'

--- igris/util/types_extension.h
+++ igris/util/types_extension.h
@@ -1,7 +1,16 @@
 #ifndef IGRIS_TYPES_EXTENSION_H
 #define IGRIS_TYPES_EXTENSION_H
 
+#include <stdint.h>
+
+#ifndef IGRIS_FLOAT32
+#define IGRIS_FLOAT32
 typedef float float32_t;
+#endif
+
+#ifndef IGRIS_FLOAT64
+#define IGRIS_FLOAT64
 typedef double float64_t;
+#endif
 
 #endif
