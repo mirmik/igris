b503eb5d55ea2709f3c299e03e6ade5f4c1dcf0c
mirmik <mirmikns@yandex.ru>
2019-07-01 02:13:29 +0300

    'a'

--- igris/math/defs.h
+++ igris/math/defs.h
@@ -3,5 +3,18 @@
 
 #define __MAX__(a,b) ((a) > (b) ? (a) : (b))
 #define __MIN__(a,b) ((a) < (b) ? (a) : (b))
+#define __ABS__(a) ((a) < 0 ? (-a) : (a))
+
+#ifndef MAX
+#define MAX(a,b) __MAX__(a,b)
+#endif
+
+#ifndef MIN
+#define MIN(a,b) __MIN__(a,b)
+#endif
+
+#ifndef ABS
+#define ABS(a) __ABS__(a)
+#endif
 
 #endif
