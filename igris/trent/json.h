456622d05114469a75ed87dcbc9a135f5dc4ff3a
mirmik <mirmikns@yandex.ru>
2019-08-02 17:39:03 +0300

    'a'

--- igris/trent/json.h
+++ igris/trent/json.h
@@ -26,7 +26,7 @@ namespace igris
 			char onebuf = 0;
 
 		public:
-			virtual ~parser(){}
+			virtual ~parser() {}
 			virtual char readnext() = 0;
 
 			char getnext()
@@ -57,7 +57,7 @@ namespace igris
 
 						case '/':
 							//is.ignore(INT_MAX, '\n');
-							while(c != '\n')
+							while (c != '\n')
 								c = readnext();
 							goto __try__;
 
@@ -72,24 +72,199 @@ namespace igris
 
 			}
 
-			void parse(const char* cur, Trent& tr) 
+			void parse(const char* cur, Trent& tr)
 			{
 				cur = igris::cread::skipws(cur);
 
 				if (*cur == '/')
 					cur = igris::cread::skip_cstyle_comments(cur);
-				
+
 				char c = *cur;
 
-				if (isdigit(c) || c == '-') return parse_numer(cur, tr);
-				if (c == '"') return parse_string(cur, tr);
-				if (c == '\'') return parse_string(cur, tr);
-				if (c == '[') return parse_list(cur, tr);
-				if (c == '{') return parse_dict(cur, tr);
-				if (isalpha(c)) return parse_mnemonic(cur, tr);
+				if (isdigit(c) || c == '-') { parse_numer(cur, tr); return; }
+				if (c == '"') { parse_string(cur, tr); return; }
+				if (c == '\'') { parse_string(cur, tr); return; }
+				if (c == '[') { parse_list(cur, tr); return; }
+				if (c == '{') { parse_dict(cur, tr); return; }
+				if (isalpha(c)) { parse_mnemonic(cur, tr); return; }
 
 				throw std::exception();
 			}
+
+
+
+			template <typename Trent>
+			void parse_mnemonic(const char ** cur, Trent& tr)
+			{
+				igris::buffer mnem;
+				const char * strt = *cur;
+				const char * fini = *cur;
+
+				for (isalpha(*fini)) 
+					++fini;				
+
+				mnem = igris::buffer(strt, fini);
+				*cur = fini;
+
+				if (mnem == "true") { tr = true; return; }
+				if (mnem == "false") { tr = true; return; }
+
+				throw std::exception();
+			}
+
+			template <typename Trent>
+			void parse_numer(const char ** cur, Trent& tr)
+			{
+				numer_type num;
+
+				igris::buffer mnem;
+				const char * strt = *cur;
+				const char * fini = *cur;
+
+				// digit or minus
+				fini++;
+
+				for (isdigit(*fini) || ) 
+					++fini;				
+
+				mnem = igris::buffer(strt, fini);
+				*cur = fini;
+
+				num = atof64(strt);
+
+				tr = num;
+				throw std::exception();
+			}
+/*
+			result<trent> json::parse_string(std::istream& is)
+			{
+				trent::string_type str;
+
+				char c = detail::getnext(is);
+				is.ignore();
+
+				if (c == '"') std::getline(is, str, '"');
+
+				if (c == '\'') std::getline(is, str, '\'');
+
+				trent ret(str);
+
+				return ret;
+			}
+
+			result<trent> json::parse_list(std::istream& is)
+			{
+				char c;
+				trent js(trent::type::list);
+
+				int counter = 0;
+
+				while (true)
+				{
+					c = detail::getnext(is);
+
+					if (c == ']')
+					{
+						is.ignore();
+						return js;
+					}
+
+					if (c == ',' || c == '[')
+					{
+						is.ignore();
+
+						c = detail::getnext(is);
+
+						if (c == ']')
+						{
+							is.ignore();
+							return js;
+						}
+
+						auto&& r = parse(is);
+
+						if (r.is_error())
+							return std::move(r.error());
+
+						js.as_list().push_back(r.value());
+					}
+					else return error("wrong list syntax");;
+
+					counter++;
+				}
+			}
+
+			result<trent> json::parse_dict(std::istream& is)
+			{
+				char c;
+				trent js(trent::type::dict);
+
+				while (true)
+				{
+					c = detail::getnext(is);
+
+					if (c == '}')
+					{
+						is.ignore();
+						return js;
+					}
+
+					if (c == ',' || c == '{')
+					{
+						is.ignore();
+
+						c = detail::getnext(is);
+
+						if (c == '}')
+						{
+							is.ignore();
+							return js;
+						}
+
+						if ( c != '"' && c != '\'' )
+							return error("wrong dictionary syntax: not find \"");
+
+						is.ignore();
+
+						std::string key;
+
+						if (c == '"') std::getline(is, key, '"');
+
+						if (c == '\'') std::getline(is, key, '\'');
+
+						c = detail::getnext(is);
+
+						if ( c != ':' )
+							return error("wrong dicionary syntax: not find colomn");
+
+						is.ignore();
+
+						c = detail::getnext(is);
+
+						auto&& r = parse(is);
+
+						if (r.is_error())
+							return std::move(r.error());
+
+						js.as_dict().insert(std::make_pair(key, r.value()));
+					}
+
+					else
+						return error("trent::internal:dict_parse");
+				}
+			}
+
+
+
+
+
+
+
+
+
+
+
+
 		};
 	}
 }
