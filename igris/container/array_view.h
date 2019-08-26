b13391a6b7e2a223ddaaf26d5744a16694b7c6a6
mirmik <mirmikns@yandex.ru>
2019-08-26 18:01:46 +0300

    'a'

---
+++ igris/container/array_view.h
@@ -0,0 +1,40 @@
+#ifndef IGRIS_CONTAINER_ARRAY_VIEW_H
+#define IGRIS_CONTAINER_ARRAY_VIEW_H
+
+namespace igris 
+{
+	template <class T>
+	class array_view 
+	{
+		T* _data;
+		size_t _size;
+
+	public:
+
+		array_view(T* data, size_t size) : _data(data), _size(size) {}
+		array_view(){}
+
+		T* const data() const { return _data; }
+		T* data() { return _data; }
+
+		size_t size() { return _size; } 
+
+		T* begin() { return _data; }
+		T* const end() { return _data + _size; }
+		const T* begin() const { return _data; }
+		const T* const end() const { return _data + _size; }
+
+		T* rbegin() { return _data + _size - 1; }
+		T* const rend() { return _data - 1; }
+		const T* rbegin() const { return _data + _size - 1; }
+		const T* const rend() const { return _data - 1; }
+
+		T& operator [] (int i) { return *(_data + i); }
+		const T& operator [] (int i) const { return *(_data + i); }
+
+		template<size_t N>
+		array_view(T(&arr)[N]) : _data(arr), _size(N) {}
+	};
+}
+
+#endif
