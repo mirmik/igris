225764e88699a8d546c88b929e5ba685cf738f39
mirmik <mirmikns@yandex.ru>
2019-10-15 22:28:33 +0300

    'merge'

--- igris/util/crc.h
+++ igris/util/crc.h
@@ -2,8 +2,7 @@
 #define GXX_UTIL_CRC_H
 
 #include <stdint.h>
-
-__BEGIN_DECLS
+#include <sys/cdefs.h>
 
 static inline void strmcrc8(uint8_t* crc, char c) 
 {
@@ -11,6 +10,8 @@ static inline void strmcrc8(uint8_t* crc, char c)
 	for (int i = 0; i < 8; i++)
 		*crc = *crc & 0x80 ? ((uint8_t)(((uint8_t)(*crc << 1)) ^ 0x31)) : ((uint8_t)(*crc << 1));
 }
+
+__BEGIN_DECLS
 
 uint8_t crc8_table(const uint8_t *addr, uint8_t len);
 uint8_t crc8(const uint8_t *addr, uint8_t len);
