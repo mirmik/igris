5252ca0c33f43de5bd0947864fe2cc635c068f96
mirmik <mirmikns@yandex.ru>
2019-11-12 15:14:07 +0300

    'travis'

--- igris/math/deg.h
+++ igris/math/deg.h
@@ -3,13 +3,21 @@
 
 #include <math.h>
 
-static constexpr double deg(double x) { return x * M_PI / 180.0; }
-static constexpr double deg2rad(double x) { return x * M_PI / 180.0; }
-static constexpr double rad2deg(double x) { return x * 180.0 / M_PI; }
-
-static constexpr float deg(float x) { return x * M_PI / 180.0; }
-static constexpr float deg2rad(float x) { return x * M_PI / 180.0; }
-static constexpr float rad2deg(float x) { return x * 180.0 / M_PI; }
+#ifdef __cplusplus
+static inline constexpr double deg(double x) { return x * M_PI / 180.0; }
+static inline constexpr double deg2rad(double x) { return x * M_PI / 180.0; }
+static inline constexpr double rad2deg(double x) { return x * 180.0 / M_PI; }
+static inline constexpr float deg(float x) { return x * M_PI / 180.0; }
+static inline constexpr float deg2rad(float x) { return x * M_PI / 180.0; }
+static inline constexpr float rad2deg(float x) { return x * 180.0 / M_PI; }
+#else
+static inline double deg(double x) { return x * M_PI / 180.0; }
+static inline double deg2rad(double x) { return x * M_PI / 180.0; }
+static inline double rad2deg(double x) { return x * 180.0 / M_PI; }
+static inline float deg(float x) { return x * M_PI / 180.0; }
+static inline float deg2rad(float x) { return x * M_PI / 180.0; }
+static inline float rad2deg(float x) { return x * 180.0 / M_PI; }
+#endif
 
 #define DEG2RAD (M_PI/180.0)
 #define RAD2DEG (180.0/M_PI)
