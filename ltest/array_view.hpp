75c28c0b1c8b14843d272277fbb725a3f66d54ef
mirmik <mirmikns@yandex.ru>
2020-07-08 16:06:56 +0300

    remove initialier_list test

--- ltest/array_view.hpp
+++ ltest/array_view.hpp
@@ -2,8 +2,8 @@
 
 LT_BEGIN_TEST(igris_test_suite, array_view_initializer_list_test)
 {
-	igris::array_view<float> arr { 0.1, 0.2 };
-	LT_CHECK_EQ(arr[0], 0.1f);
-	LT_CHECK_EQ(arr[1], 0.2f);
+//	igris::array_view<float> arr { 0.1, 0.2 };
+//	LT_CHECK_EQ(arr[0], 0.1f);
+//	LT_CHECK_EQ(arr[1], 0.2f);
 }
 LT_END_TEST(array_view_initializer_list_test)
