12988d6c9b7eb7a31e0a816942beda5a6be7ab6b
mirmik <mirmikns@yandex.ru>
2020-08-24 11:07:06 +0300

    'osutil'

--- igris/osutil/getcwd.h
+++ igris/osutil/getcwd.h
@@ -1,27 +1,13 @@
-#ifndef GXX_OSUTIL_GETCWD_H
-#define GXX_OSUTIL_GETCWD_H
+#ifndef IGRIS_OSUTIL_GETCWD_H
+#define IGRIS_OSUTIL_GETCWD_H
 
-#include <unistd.h>
-#include <string.h>
 #include <string>
 
 namespace igris
 {
   namespace osutil
   {
-    static inline std::string getcwd()
-    {
-      char cwd[1024];
-      if (::getcwd(cwd, sizeof(cwd)) != NULL)
-      {
-        return std::string(cwd, strlen(cwd));
-      }
-      else
-      {
-        perror("getcwd() error");
-        return std::string();
-      }
-    }
+    std::string getcwd();
   }
 }
 
