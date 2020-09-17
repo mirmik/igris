3bfac253acab769ccf8560ba1192dd91dffedfff
mirmik <mirmikns@yandex.ru>
2020-09-17 11:36:38 +0300

    'clean'

--- igris/event/HIDE/event.h
+++ igris/event/HIDE/event.h
@@ -1,40 +1,12 @@
-#ifndef GXX_EVENT_H
-#define GXX_EVENT_H
+#ifndef IGRIS_EVENT_EVENT_H
+#define IGRIS_EVENT_EVENT_H
 
-#include <gxx/dlist.h>
-#include <gxx/pool.h>
+#include <igris/event/delegate.h>
 
-namespace gxx {
-	namespace event {
-		constexpr uint8_t SelfDestruct = 0x01;
-		constexpr uint8_t Nothing = 0x01;
-		constexpr uint8_t Unbind = 0x01;
-
-		class event_processor {
-
-		};
-
-		template <typename ... Args>
-		class node {
-		public:
-			bool en;
-			gxx::delegate<void, node*, Args ...>
-		};
-
-		class event {
-		public:
-			dlist<node, &node::lnk> list;
-		};
-
-		class flag : public event {
-		public:
-		};
-
-		extern gxx::pool<node> node_pool;
-
-		node* get_event_node();
-		void  put_event_node(node*);
-	}
+namespace igris 
+{
+	class event_args {};
+	using event = igris::delegate<void, void*>;
 }
 
 #endif
