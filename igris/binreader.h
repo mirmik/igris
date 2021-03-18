f5c0d63a37891dfb0ff285e263b02b94bcc8a952
mirmik <mirmikns@yandex.ru>
2021-03-19 01:08:55 +0300

    clang-format

--- igris/binreader.h
+++ igris/binreader.h
@@ -1,5 +1,5 @@
 /**
-	Однопроходной парсинг последовательных данных.	
+    Однопроходной парсинг последовательных данных.
 */
 
 #ifndef IGRIS_BINREADER_H
@@ -10,9 +10,9 @@
 
 __BEGIN_DECLS
 
-void reader_memcpy(const char ** ptr, void* obj, size_t objsize);
-void reader_skip(const char ** ptr, size_t size) ;
-void reader_skip_while(const char ** ptr, char c, const char* protector);
+void reader_memcpy(const char **ptr, void *obj, size_t objsize);
+void reader_skip(const char **ptr, size_t size);
+void reader_skip_while(const char **ptr, char c, const char *protector);
 
 __END_DECLS
 
@@ -20,55 +20,50 @@ __END_DECLS
 #include <igris/buffer.h>
 #include <igris/util/numconvert.h>
 
-namespace igris 
+namespace igris
 {
-	class binreader 
-	{
-		const char * ptr;
+    class binreader
+    {
+        const char *ptr;
 
-	public:
-		binreader (){}
-		binreader (const char * ptr) : ptr(ptr) {}
+      public:
+        binreader() {}
+        binreader(const char *ptr) : ptr(ptr) {}
 
-		void init(const char * ptr) { this->ptr = ptr; }
+        void init(const char *ptr) { this->ptr = ptr; }
 
-		template <class T> 
-		void read_binary(T& obj) 
-		{
-			memcpy(&obj, ptr, sizeof(T));
-			ptr += sizeof(T);
-		} 
+        template <class T> void read_binary(T &obj)
+        {
+            memcpy(&obj, ptr, sizeof(T));
+            ptr += sizeof(T);
+        }
 
-		template <class Pointer>
-		void bind_buffer(Pointer*& data, size_t sz) 
-		{
-			data = (Pointer*) ptr;
-			ptr += sz;
-		}
+        template <class Pointer> void bind_buffer(Pointer *&data, size_t sz)
+        {
+            data = (Pointer *)ptr;
+            ptr += sz;
+        }
 
-		void bind_buffer(igris::buffer& data, size_t sz) 
-		{
-			data = { ptr, sz };
-			ptr += sz;
-		}
+        void bind_buffer(igris::buffer &data, size_t sz)
+        {
+            data = {ptr, sz};
+            ptr += sz;
+        }
 
-		void skip(int n) 
-		{
-			ptr += n;
-		}
+        void skip(int n) { ptr += n; }
 
-		int read_ascii_decimal_integer(int * ret) 
-		{
-			*ret = atoi32(ptr, 10, (char**)&ptr);
-			return 0;
-		}
+        int read_ascii_decimal_integer(int *ret)
+        {
+            *ret = atoi32(ptr, 10, (char **)&ptr);
+            return 0;
+        }
 
-		int read_ascii_decimal_float(float * ret) 
-		{
-			*ret = atof32(ptr, (char**)&ptr);
-			return 0;
-		}
-	};
+        int read_ascii_decimal_float(float *ret)
+        {
+            *ret = atof32(ptr, (char **)&ptr);
+            return 0;
+        }
+    };
 }
 #endif
 
