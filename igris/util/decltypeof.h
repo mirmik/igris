0929921cedbdaaecd432e91823936126dc71cce2
mirmik <mirmikns@yandex.ru>
2021-04-13 12:30:13 +0300

    'remove depends from std'

--- igris/util/decltypeof.h
+++ igris/util/decltypeof.h
@@ -2,10 +2,23 @@
 #define IGRIS_DECLTYPEOF_H
 
 //Макрос decltypeof имитирует поведение typeof в С/С++ коде.
-
 #if __cplusplus
-#include <utility>
-#define decltypeof(a) typename std::remove_reference<decltype(a)>::type
+namespace
+{
+    template <class T> struct remove_reference
+    {
+        typedef T type;
+    };
+    template <class T> struct remove_reference<T &>
+    {
+        typedef T type;
+    };
+    template <class T> struct remove_reference<T &&>
+    {
+        typedef T type;
+    };
+}
+#define decltypeof(a) typename remove_reference<decltype(a)>::type
 #else
 #define decltypeof(a) typeof(a)
 #endif
