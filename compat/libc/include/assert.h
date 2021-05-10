aa40322e09811957b0ae0a3e658e7e3f6c4233ce
mirmik <mirmikns@yandex.ru>
2021-05-10 14:33:56 +0300

    'a'

--- compat/libc/include/assert.h
+++ compat/libc/include/assert.h
@@ -2,25 +2,28 @@
 #define ASSERT_H_
 
 #include <igris/dprint/dprint.h>
-#include <igris/util/emergency_stop.h>
+#include <igris/util/emergency_halt.h>
 
 #ifndef NDEBUG
-static inline void __assert(const char* message, const char* file, int line) 
-{ 
-	dprln("assert: "); 
-	DPRINT(message); 
-	DPRINT(file); 
-	DPRINT(line); 
-	emergency_stop();
-	while(1); 
+static inline void __assert(const char *message, const char *file, int line)
+{
+    dprln("assert: ");
+    DPRINT(message);
+    DPRINT(file);
+    DPRINT(line);
+    emergency_halt();
+    while (1)
+        ;
 }
-#define assert(EX) if (!(EX)) __assert (#EX, __FILE__, __LINE__)
+#define assert(EX)                                                             \
+    if (!(EX))                                                                 \
+    __assert(#EX, __FILE__, __LINE__)
 #else
 #define assert(EX)
 #endif
 
 #ifndef __cplusplus
-#	define static_assert(a,b) _Static_assert(a,b)
+#define static_assert(a, b) _Static_assert(a, b)
 #endif
 
 #endif /* ASSERT_H_ */
---
+++ igris/util/emergency_halt.h
@@ -0,0 +1,8 @@
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+// реализация авариёного отключения выполняется пользовательским кодом.
+void emergency_halt();
+
+__END_DECLS
