88f0f645fc1c04a28e2ca99396ac5b4e93375b5b
mirmik <mirmikns@yandex.ru>
2021-06-05 11:01:43 +0300

    'clean'

--- igris/container/dlist.h
+++ igris/container/dlist.h
@@ -19,7 +19,6 @@ namespace igris
                                               type, ptrdiff_t, type *, type &>
         {
           public:
-            // using iterator_category = std::bidirectional_iterator_tag;
             using value_type = type;
             using difference_type = ptrdiff_t;
             using pointer = type *;
@@ -109,60 +108,41 @@ namespace igris
 
         // METHODS:
         dlist() { dlist_init(&list); }
-
         ~dlist() { dlist_del(&list); }
 
-        bool empty() { return dlist_empty(&list); };
-
+        bool empty() { return dlist_empty(&list); }
         type *first() { return member_container(list.next, member); }
 
-        //Прилинковать первым в списке.
-        void move_front(type &obj) { dlist_move_next(&(obj.*member), &list); };
-
-        //Прилинковать последним в списке.
-        void move_back(type &obj) { dlist_move_prev(&(obj.*member), &list); };
-
         void add_first(type &obj) { dlist_add(&(obj.*member), &list); }
-
         void add_last(type &obj) { dlist_add_tail(&(obj.*member), &list); }
 
-        //Поставить объект перед объектом.
+        void move_front(type &obj) { dlist_move_next(&(obj.*member), &list); }
+        void move_back(type &obj) { dlist_move_prev(&(obj.*member), &list); }
         void move_next(type &obj, type &head)
         {
-            dlist_move_next(&(obj.*member), &(head.*member));
-        };
-
-        //Поставить объект перед итерируемым объектом.
+            dlist_move_next(&(obj.*member), &(head.*member))
+        }
         void move_next(type &obj, iterator head)
         {
             dlist_move_next(&(obj.*member), head.current);
-        };
+        }
 
-        //Поставить объект после объекта.
         void move_prev(type &obj, type &head)
         {
             dlist_move_prev(&(obj.*member), &(head.*member));
-        };
-
-        //Поставить объект после итерируемого объектом.
+        }
         void move_prev(type &obj, iterator head)
         {
             dlist_move_prev(&(obj.*member), head.current);
-        };
+        }
 
-        //      iterator insert(iterator it, type & obj) {
-        //          dlist_move_prev(&(obj.*member), it.current);
-        //      }
+        void pop(type &obj) { dlist_del(&(obj.*member)); }
+        void pop_front() { dlist_del(&((*begin()).*member)); }
+        void pop_back() { dlist_del(&((*rbegin()).*member)); }
 
-        void pop(type &obj)
-        {
-            // assert(is_linked(obj));
-            dlist_del(&(obj.*member));
-        };
+        static void unbind(type &obj) { dlist_del(&(obj.*member)); }
 
-        static void unbind(type &obj) { dlist_del(&(obj.*member)); };
-
-        void del_init(type &obj) { dlist_del_init(&(obj.*member)); };
+        void del_init(type &obj) { dlist_del_init(&(obj.*member)); }
 
         void pop_if_linked(type &obj)
         {
@@ -171,11 +151,7 @@ namespace igris
             dlist_del(&(obj.*member));
         };
 
-        void pop_front() { dlist_del(&((*begin()).*member)); };
-
-        void pop_back() { dlist_del(&((*rbegin()).*member)); };
-
-        void round_left() { move_back(*begin()); };
+        void round_left() { move_back(*begin()); }
 
         int size() const
         {
@@ -185,7 +161,7 @@ namespace igris
                 i++;
             }
             return i;
-        };
+        }
 
         iterator begin() { return iterator(list.next); }
         iterator end() { return iterator(&list); }
@@ -195,48 +171,7 @@ namespace igris
 
         reverse_iterator rbegin() { return reverse_iterator(list.prev); }
         reverse_iterator rend() { return reverse_iterator(&list); }
-
-        // iterator insert_sorted(type & item) {
-        //    return insert(std::upper_bound(begin(), end(), item ), item);
-        //}
-
-        /*  gxx::string to_info() const
-            {
-                gxx::string str;
-                str.reserve(128);
-                int i = 0;
-                dlist_head* ptr;
-                dlist_for_each(ptr, &list)
-                {
-                    i++;
-                };
-                str << "count: " << i;
-                return str;
-            };
-        */
-        /*  gxx::string to_str() const
-            {
-                gxx::string str;
-                str.reserve(128);
-                str << "[";
-                for (auto r : *this)
-                {
-                    str << to_str(r) << gxx::string(",");
-                };
-                str << "]";
-                return str;
-            };
-        */
     };
 }
-
-/*namespace std {
-    template<typename T, dlist_head T::* L>
-    class iterator_traits<typename gxx::dlist<T,L>::iterator> {
-    public:
-        using iterator_category = std::bidirectional_iterator_tag;
-        using value_type = T;
-    };
-}*/
 
 #endif
