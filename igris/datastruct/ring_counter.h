8f5985e50286632bd0a19534085ea24b3d7d3946
mirmik <mirmikns@yandex.ru>
2021-06-14 20:23:48 +0300

    'tuple'

--- igris/datastruct/ring_counter.h
+++ igris/datastruct/ring_counter.h
@@ -7,15 +7,20 @@ struct ring_counter
 {
     int counter;
     int size;
-}
+};
 
-__BEGIN_CDECLS
+__BEGIN_DECLS
 
-    static inline void
-    ring_counter_init(struct ring_counter *rc, int size)
+static inline void ring_counter_init(struct ring_counter *rc, int size)
 {
     rc->counter = 0;
     rc->size = size;
+}
+
+static inline void ring_counter_fixup(struct ring_counter *rc)
+{
+    while (rc->counter >= rc->size)
+        rc->counter -= rc->size;
 }
 
 static inline void ring_counter_set(struct ring_counter *rc, int val)
@@ -24,20 +29,28 @@ static inline void ring_counter_set(struct ring_counter *rc, int val)
     ring_counter_fixup(rc);
 }
 
-static inline void ring_counter_fixup(struct ring_counter *rc)
-{
-    while (rc->counter >= r->size)
-        r->counter -= r->size;
-}
-
 static inline void ring_counter_increment(struct ring_counter *rc, int arg)
 {
     rc->counter += arg;
     ring_counter_fixup(rc);
 }
 
-int ring_counter_get(struct ring_counter *rc) { return rc->counter; }
+static inline int ring_counter_get(struct ring_counter *rc)
+{
+    return rc->counter;
+}
 
-__END_CDECLS
+/// Вернуть номер, бывший на i итераций ранее.
+static inline int ring_counter_prev(struct ring_counter *rc, int i)
+{
+    int c = rc->counter - i;
+
+    while (c < 0)
+        c += rc->size;
+
+    return c;
+}
+
+__END_DECLS
 
 #endif
