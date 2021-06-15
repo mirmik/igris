ee114999d1637745c1cc47cb4c67707c2c8bcdbf
mirmik <mirmikns@yandex.ru>
2021-06-15 11:17:22 +0300

    'a'

--- igris/time/time_posix.cpp
+++ igris/time/time_posix.cpp
@@ -26,10 +26,30 @@ int msleep(int64_t m)
 }
 
 
-double igris::time()
+/*double igris::time()
 {
     auto duration = std::chrono::system_clock::now().time_since_epoch();
     auto ns =
         std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
-    return (double)ns / 1e9;
+    return (double)ns / 1.e9;
+}*/
+
+int64_t igris::nanos()
+{
+    auto duration = std::chrono::system_clock::now().time_since_epoch();
+    auto ns =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
+    return ns;
+}
+
+static int64_t __local_time = igris::nanos();
+
+void igris::start_local_time() 
+{
+    __local_time = igris::nanos();
+}
+
+double igris::local_time() 
+{
+    return (double)((igris::nanos() - __local_time)*1e-9);
 }
