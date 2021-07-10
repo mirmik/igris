cc35dd7f7892e543b9d5a91bbbb12ccafff576db
mirmik <mirmikns@yandex.ru>
2021-07-10 11:05:25 +0300

    arduino binding

---
+++ bindings/arduino/collect_sources.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import glob
+
+os.makedirs("src/igris", exist_ok=True)
+
+PREFIX = "../../igris"
+
+SOURCES = [
+	"dprint/dprint_func_impl.c",
+	"dprint/dprint_manually.c"
+]
+
+for path, subdirs, files in os.walk(PREFIX):
+    for f in [ f for f in files if os.path.splitext(f)[1] == ".h" ]:
+    	difpath = os.path.relpath(path, PREFIX)
+    	os.makedirs(os.path.join("src/igris", difpath), exist_ok=True)
+    	shutil.copyfile(src=os.path.join(PREFIX, difpath, f), dst=os.path.join("src/igris", difpath, f))
+
+for s in SOURCES:
+	src = os.path.join(PREFIX, s)
+	name = os.path.basename(s)
+
+	shutil.copyfile(src, os.path.join("src", name))
+
+with open("src/igris.h", "w") as f:
+	f.write("//PLACEHOLDER\r\n")
