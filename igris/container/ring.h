22360ab06a2854c83a164a14df81c362aa4608a3
mirmik <mirmikns@yandex.ru>
2021-08-25 18:46:08 +0300

    'newformat rule'

--- igris/container/ring.h
+++ igris/container/ring.h
@@ -16,16 +16,24 @@ namespace igris
      * С целью оптимизации разрешает доступ к внутренним переменным и прямое
      * управление буффером.
      * */
-    template <typename T> class ring
+    template <typename T>
+    class ring
     {
       public:
         ring_head r;
+        int size;
         T *buffer;
 
       public:
-        ring(T *buffer, int size) : buffer(buffer) { ring_init(&r, size); }
+        ring(T *buffer, int bufsize) : buffer(buffer), size(0)
+        {
+            ring_init(&r, bufsize);
+        }
 
-        bool empty() { return ring_empty(&r); }
+        bool empty()
+        {
+            return ring_empty(&r);
+        }
 
         void push(const T &obj)
         {
@@ -33,7 +41,8 @@ namespace igris
             ring_move_head_one(&r);
         }
 
-        template <typename... Args> void emplace(Args &&... args)
+        template <typename... Args>
+        void emplace(Args &&... args)
         {
             new (buffer + r.head) T(std::forward<Args>(args)...);
             ring_move_head_one(&r);
@@ -46,17 +55,35 @@ namespace igris
             ring_move_tail_one(&r);
         }
 
-        void move_tail_one() { ring_move_tail_one(&r); }
+        void move_tail_one()
+        {
+            ring_move_tail_one(&r);
+        }
 
-        void move_head_one() { ring_move_tail_one(&r); }
+        void move_head_one()
+        {
+            ring_move_tail_one(&r);
+        }
 
-        unsigned int avail() { return ring_avail(&r); }
+        unsigned int avail()
+        {
+            return ring_avail(&r);
+        }
 
-        unsigned int room() { return ring_room(&r); }
+        unsigned int room()
+        {
+            return ring_room(&r);
+        }
 
-        T &get(int index) { return buffer[index]; }
+        T &get(int index)
+        {
+            return buffer[index];
+        }
 
-        T &last() { return buffer[r.tail]; }
+        T &last()
+        {
+            return buffer[r.tail];
+        }
     };
 }
 
