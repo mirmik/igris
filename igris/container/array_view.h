eb1d7a37fa1b575895c86bbb984bd5384b654a18
mirmik <mirmikns@yandex.ru>
2021-08-27 14:51:31 +0300

    'array_view constructor'

--- igris/container/array_view.h
+++ igris/container/array_view.h
@@ -7,7 +7,8 @@
 
 namespace igris
 {
-    template <class T> class array_view
+    template <class T>
+    class array_view
     {
         T *_data;
         size_t _size;
@@ -18,8 +19,15 @@ namespace igris
         using value_type = T;
 
       public:
-        array_view(T *data, size_t size) : _data(data), _size(size) {}
-        array_view() : _data(nullptr), _size(0) {}
+        array_view(T *data, size_t size) : _data(data), _size(size)
+        {
+        }
+        array_view(T *data, int size) : _data(data), _size((size_t)size)
+        {
+        }
+        array_view() : _data(nullptr), _size(0)
+        {
+        }
 
         array_view(const std::initializer_list<T> &lst)
         {
@@ -27,7 +35,10 @@ namespace igris
             _size = lst.size();
         }
 
-        template <size_t N> array_view(T (&arr)[N]) : _data(arr), _size(N) {}
+        template <size_t N>
+        array_view(T (&arr)[N]) : _data(arr), _size(N)
+        {
+        }
 
         // Warning: UB
         /*		array_view(const std::initializer_list<T>& lst) :
@@ -41,23 +52,62 @@ namespace igris
             return *this;
         }
 
-        T *const data() const { return _data; }
-        T *data() { return _data; }
+        T *const data() const
+        {
+            return _data;
+        }
+        T *data()
+        {
+            return _data;
+        }
 
-        size_t size() const { return _size; }
+        size_t size() const
+        {
+            return _size;
+        }
 
-        T *begin() { return _data; }
-        T *const end() { return _data + _size; }
-        const T *begin() const { return _data; }
-        const T *const end() const { return _data + _size; }
+        T *begin()
+        {
+            return _data;
+        }
+        T *const end()
+        {
+            return _data + _size;
+        }
+        const T *begin() const
+        {
+            return _data;
+        }
+        const T *const end() const
+        {
+            return _data + _size;
+        }
 
-        T *rbegin() { return _data + _size - 1; }
-        T *const rend() { return _data - 1; }
-        const T *rbegin() const { return _data + _size - 1; }
-        const T *const rend() const { return _data - 1; }
+        T *rbegin()
+        {
+            return _data + _size - 1;
+        }
+        T *const rend()
+        {
+            return _data - 1;
+        }
+        const T *rbegin() const
+        {
+            return _data + _size - 1;
+        }
+        const T *const rend() const
+        {
+            return _data - 1;
+        }
 
-        T &operator[](int i) { return *(_data + i); }
-        const T &operator[](int i) const { return *(_data + i); }
+        T &operator[](int i)
+        {
+            return *(_data + i);
+        }
+        const T &operator[](int i) const
+        {
+            return *(_data + i);
+        }
 
         array_view slice(int start, int size)
         {
