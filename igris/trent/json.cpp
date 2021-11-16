3b3f19c3b3831877453cc96e33e967cb61835c29
mirmik <mirmikns@yandex.ru>
2021-11-16 12:30:50 +0300

    'parse_file'

--- igris/trent/json.cpp
+++ igris/trent/json.cpp
@@ -1,4 +1,4 @@
-
+#include <fstream>
 #include <igris/trent/json.h>
 
 igris::trent igris::json::parse(const char *str)
@@ -11,4 +11,16 @@ igris::trent igris::json::parse(const std::string &str)
 {
     parser_str parser(str);
     return parser.parse();
+}
+
+igris::trent igris::json::parse_file(const std::string &str)
+{
+    std::ifstream t("file.txt");
+    t.seekg(0, std::ios::end);
+    size_t size = t.tellg();
+    std::string buffer(size, ' ');
+    t.seekg(0);
+    t.read(&buffer[0], size);
+
+    return parse(buffer);
 }
