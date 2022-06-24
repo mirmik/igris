bc45b8b07e313905ead231d44d59dc50665be6f6
mirmik <mirmik@yandex.ru>
2022-06-24 18:43:38 +0300

    'a'

--- make.py
+++ make.py
@@ -10,7 +10,7 @@ licant.execute("igris.g.py")
 licant.cli.add_argument("--toolchain")
 opts, args = licant.cli.parse()
 
-if hasattr(licant, 'gcc_toolchain'):
+if hasattr(licant, 'gcc_toolchain'): 
 	toolchain = licant.cxx_make.gcc_toolchain(opts.toolchain)
 else:
 	toolchain = licant.cxx_make.toolchain_gcc(opts.toolchain)
