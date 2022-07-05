80cba9c01247cf05f1a3a40252f5ed9aea6c7af8
mirmik <mirmikns@yandex.ru>
2022-07-05 12:06:48 +0300

    Fix __print test

--- tests/printf.cpp
+++ tests/printf.cpp
@@ -12,7 +12,7 @@ static void sprint_printchar(void *d, int c)
     *(((struct sprint_char_handler_data *)d)->cursor)++ = c;
 }
 
-static int vsprintf(char *s, const char *format, va_list ap)
+static int test_vsprintf(char *s, const char *format, va_list ap)
 {
     int ret;
 
@@ -24,53 +24,53 @@ static int vsprintf(char *s, const char *format, va_list ap)
     return ret;
 }
 
-static int sprintf(char *buf, const char *format, ...)
+static int test_sprintf(char *buf, const char *format, ...)
 {
     int ret;
     va_list args;
 
     va_start(args, format);
-    ret = vsprintf(buf, format, args);
+    ret = test_vsprintf(buf, format, args);
     va_end(args);
 
     return ret;
 }
 
-static int snprintf(char *buf, size_t maxlen, const char *format, ...)
+/*static int test_snprintf(char *buf, size_t maxlen, const char *format, ...)
 {
     int ret;
     va_list args;
 
     va_start(args, format);
-    ret = vsprintf(buf, format, args);
+    ret = test_vsprintf(buf, format, args);
     va_end(args);
 
     return ret;
-}
+}*/
 
-static void prflt(char * buf, float fl) 
+static void test_prflt(char * buf, float fl) 
 {
-	sprintf(buf, "%lf", fl);
+	test_sprintf(buf, "%lf", fl);
 } 
 
-static void prdbl(char * buf, double fl) 
+static void test_prdbl(char * buf, double fl) 
 {
-	sprintf(buf, "%lf", fl);
+	test_sprintf(buf, "%lf", fl);
 } 
 
 TEST_CASE("sprintf") 
 {
 	char buf[48];
     memset(buf, 0, 48);
-	sprintf(buf, "%d", 42);
+	test_sprintf(buf, "%d", 42);
 	CHECK(strcmp("42", buf) == 0);
 
-	sprintf(buf, "%ld", 42);
+	test_sprintf(buf, "%ld", 42);
 	CHECK(strcmp("42", buf) == 0);
 
-	prflt(buf, 42.250);
+	test_prflt(buf, 42.250);
 	CHECK(strcmp("42.250000", buf) == 0);
 
-	prdbl(buf, 42.250);
+	test_prdbl(buf, 42.250);
 	CHECK(strcmp("42.250000", buf) == 0);
 }
