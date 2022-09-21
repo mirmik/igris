27792a1ad1de3d5ffbcbd0e30acbf97522584704
mirmik <mirmikns@yandex.ru>
2022-09-21 11:50:35 +0300

    Add new classes to igris_std

--- igris/util/ctrdtr.h
+++ igris/util/ctrdtr.h
@@ -2,25 +2,29 @@
 #define IGRIS_UTIL_CTRDTR_H
 
 #include <new>
+#include <utility>
 
 namespace igris
 {
-    template <typename T> void destructor(T *ptr) { ptr->~T(); }
+    template <typename T> void destructor(T *ptr)
+    {
+        ptr->~T();
+    }
 
     template <typename T, typename... Args>
     void constructor(T *ptr, Args &&... args)
     {
-        new ((void*)ptr) T(std::forward<Args>(args)...);
+        new ((void *)ptr) T(std::forward<Args>(args)...);
     }
 
     template <typename T> void copy_constructor(T *ptr, const T &other)
     {
-        new ((void*)ptr) T(other);
+        new ((void *)ptr) T(other);
     }
 
     template <typename T> void move_constructor(T *ptr, T &&other)
     {
-        new ((void*)ptr) T(std::move(other));
+        new ((void *)ptr) T(std::move(other));
     }
 
     template <class InputIterator, class EndIterator>
