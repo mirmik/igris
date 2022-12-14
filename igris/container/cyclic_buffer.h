8ebbc1e6d32894de1c09c2d62d4a069f183f8d50
mirmik <mirmikns@yandex.ru>
2022-12-14 23:36:58 +0300

    'add const'

--- igris/container/cyclic_buffer.h
+++ igris/container/cyclic_buffer.h
@@ -1,15 +1,15 @@
 #ifndef RALGO_CYCLIC_BUFFER_H
 #define RALGO_CYCLIC_BUFFER_H
 
-#include <igris/datastruct/ring_counter.h>
 #include <igris/container/unbounded_array.h>
+#include <igris/datastruct/ring_counter.h>
 
 namespace igris
 {
-    template <class T, class Alloc=std::allocator<T>> class cyclic_buffer
+    template <class T, class Alloc = std::allocator<T>> class cyclic_buffer
     {
     public:
-        igris::unbounded_array<T,Alloc> data = {};
+        igris::unbounded_array<T, Alloc> data = {};
         ring_counter counter = {};
         size_t _size = 0;
 
@@ -18,14 +18,15 @@ namespace igris
             ring_counter_init(&counter, size);
         }
 
-        size_t size() 
-        { 
-            return _size; 
+        size_t size() const
+        {
+            return _size;
         }
 
         T push(T val)
         {
-            if (_size < (size_t)counter.size) _size++;
+            if (_size < (size_t)counter.size)
+                _size++;
 
             ring_counter_increment(&counter, 1);
             T ret = data[ring_counter_get(&counter)];
@@ -36,14 +37,15 @@ namespace igris
         /// Получить элемент z*i, где z - дискретный оператор.
         /// 0 - последний, 1 - предпоследний
         // и так далее
-        T operator[](int i) { 
-            return data[ring_counter_prev(&counter, i)]; 
+        T operator[](int i)
+        {
+            return data[ring_counter_prev(&counter, i)];
         }
 
-        void resize(size_t size) 
+        void resize(size_t size)
         {
             data.resize(size);
-            ring_counter_init(&counter, size);            
+            ring_counter_init(&counter, size);
         }
     };
 }
