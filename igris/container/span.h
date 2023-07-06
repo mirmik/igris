ad3ed64529616bb184973358fb3e87d0b1c887e5
mirmik <mirmikns@yandex.ru>
2023-07-06 12:35:22 +0300

    'span'

---
+++ igris/container/span.h
@@ -0,0 +1,92 @@
+#ifndef STD_SPAN_H
+#define STD_SPAN_H
+
+#include <cstddef>
+
+namespace igris
+{
+    template <class T> class span
+    {
+        T *_data;
+        size_t _size;
+
+    public:
+        span() : _data(nullptr), _size(0) {}
+        span(T *data, size_t size) : _data(data), _size(size) {}
+
+        T *data()
+        {
+            return _data;
+        }
+        const T *data() const
+        {
+            return _data;
+        }
+
+        size_t size() const
+        {
+            return _size;
+        }
+        size_t size_bytes() const
+        {
+            return _size * sizeof(T);
+        }
+
+        T &operator[](size_t index)
+        {
+            return _data[index];
+        }
+        const T &operator[](size_t index) const
+        {
+            return _data[index];
+        }
+
+        T &front()
+        {
+            return _data[0];
+        }
+
+        T &back()
+        {
+            return _data[_size - 1];
+        }
+
+        span<T> subspan(size_t offset, size_t count)
+        {
+            return span<T>(_data + offset, count);
+        }
+
+        span<T> subspan(size_t offset)
+        {
+            return span<T>(_data + offset, _size - offset);
+        }
+
+        span<T> first(size_t count)
+        {
+            return span<T>(_data, count);
+        }
+
+        span<T> last(size_t count)
+        {
+            return span<T>(_data + _size - count, count);
+        }
+
+        bool empty() const
+        {
+            return _size == 0;
+        }
+
+        void remove_prefix(size_t count)
+        {
+            _data += count;
+            _size -= count;
+        }
+
+        void remove_suffix(size_t count)
+        {
+            _size -= count;
+        }
+    };
+}
+
+#endif
---
+++ tests/span.cpp
@@ -0,0 +1,17 @@
+#include <array>
+#include <doctest/doctest.h>
+#include <igris/container/span.h>
+
+TEST_CASE("span")
+{
+    std::array<int, 5> arr = {1, 2, 3, 4, 5};
+    igris::span<int> spn(arr.data(), arr.size());
+
+    CHECK(spn.size() == 5);
+    CHECK(spn.size_bytes() == 5 * sizeof(int));
+    CHECK(spn[0] == 1);
+    CHECK(spn[1] == 2);
+    CHECK(spn[2] == 3);
+    CHECK(spn[3] == 4);
+    CHECK(spn[4] == 5);
+}
