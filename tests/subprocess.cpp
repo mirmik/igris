5d8e9716e19e4e3036e211e6810d0eab7d16da39
mirmik <mirmikns@yandex.ru>
2023-09-01 14:39:22 +0300

    'remove subprocess test'

--- tests/subprocess.cpp
+++ tests/subprocess.cpp
@@ -13,7 +13,7 @@ TEST_CASE("SCLONNER")
     proc.exec("/bin/cat tests/text.txt");
     proc.wait();
 
-    int r = read(proc.output_fd(), txt, 48);
+    read(proc.output_fd(), txt, 48);
     // CHECK_EQ(r, 7);
     // CHECK_EQ(strcmp(txt, "AAABBB\n"), 0);
 }
