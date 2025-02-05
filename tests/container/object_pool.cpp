c2969a3253cf2130a0c2e5a55876fb91a15664ba
nfsorokin <sorokin@smitek-robotics.com>
2025-02-05 16:42:15 +0300

    upload-artifacts

--- tests/container/object_pool.cpp
+++ tests/container/object_pool.cpp
@@ -2,16 +2,14 @@
 #include <igris/container/static_object_pool.h>
 #include <vector>
 
-#include <nos/print.h>
-
 TEST_CASE("object_pool : 1")
 {
     igris::static_object_pool<int, 4> pool;
     CHECK_EQ(pool.avail(), 4);
 
-    int* a = pool.create(5);
-    int* b = pool.create(6);
-    int* c = pool.create(7);
+    int *a = pool.create(5);
+    int *b = pool.create(6);
+    int *c = pool.create(7);
 
     CHECK(*a == 5);
     CHECK(*b == 6);
@@ -25,12 +23,12 @@ TEST_CASE("object_pool : 1")
 
     CHECK_EQ(pool.avail(), 4);
 
-    int * d = pool.create(8);
-    int * e = pool.create(9);
-    int * f = pool.create(10);
-    int * g = pool.create(11);
+    int *d = pool.create(8);
+    int *e = pool.create(9);
+    int *f = pool.create(10);
+    int *g = pool.create(11);
 
-    int * h = pool.create(12);
+    int *h = pool.create(12);
 
     CHECK_EQ(pool.avail(), 0);
 
